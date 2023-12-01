48feccddff2562c1920fe7bd0dfa7f96e6218ae2
Edidiong Ukem <edidionge.ukem@gmail.com>
2023-12-01 01:45:28 +0000

    triangle

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''A module for working with Pascal's triangle.
+'''
+
+
+def pascal_triangle(n):
+    '''Creates a list of lists of integers representing
+    the Pascal's triangle of a given integer.
+    '''
+    if n <= 0:
+        return []
+    triangle = [[1]*(i+1) for i in range(n)]
+    for i in range(n):
+        for j in range(1, i):
+            triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j]
+    return triangle
